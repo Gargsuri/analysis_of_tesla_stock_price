dd632b3a3039728f7968a0b6ecf417367a5eff5c
Gargsuri <51432906+Gargsuri@users.noreply.github.com>
2021-03-01 12:20:50 +0530

    Add files via upload

---
+++ tidy.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(tidyr)
+tsla<-read.csv("C:/Users/nikag/Downloads/TSLA.csv")
+str(tsla)
+
+table (complete.cases(tsla))
+summary  (tsla [!complete.cases(tsla),])
+
+tsla<-tsla %>%
+  pivot_longer(cols = c(`High`, `Low`), names_to = "Result", values_to = "Value")
+tsla
+tsla<-tsla %>%
+  separate(
+    col =Date,
+    into = c("Year", "Month","Date"),
+    convert = TRUE
+  )
+tsla
+tsla %>% 
+  unite(col = "Date", Year,Month,Date)
+
